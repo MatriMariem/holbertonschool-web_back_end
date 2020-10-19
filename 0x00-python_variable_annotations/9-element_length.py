ea4774307e212ff9b590b727dde5a4cb04d01add
MatriMariem <meriemmatri1994@gmail.com>
2020-10-19 21:10:25 +0100

    fix pep8 style

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ a type-annotated function element_length """
-from typing import Iterable, List, Sequence,Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
