c913a0e2047223430d20b1784e6fec26ec41f397
MatriMariem <meriemmatri1994@gmail.com>
2020-10-25 16:32:19 +0100

    fifo cache replacement policy

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" FIFOCache """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache Class """
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data
+        the item value for the key key
+        """
+        abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+            else:
+                if len(self.cache_data) >= self.MAX_ITEMS:
+                    for i in abc:
+                        if i in self.cache_data:
+                            break
+                    del self.cache_data[i]
+                    print("DISCARD:", i)
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value of key in self.cache_data
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
