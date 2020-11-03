bec6131438a83a8f9e8b7288261628b440ebd9df
MatriMariem <meriemmatri1994@gmail.com>
2020-11-04 00:39:30 +0100

    encrypting password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ Encrypting passwords
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """ Encrypting passwords """
     p = password.encode()
     return bcrypt.hashpw(p, bcrypt.gensalt())
