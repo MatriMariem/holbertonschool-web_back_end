ea5df7431c9c97d8e937502ec31880b10f576e05
MatriMariem <meriemmatri1994@gmail.com>
2020-11-04 22:19:30 +0100

    Task 2 : create a logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -48,6 +48,7 @@ def get_logger() -> logging.Logger:
     """
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
+    logger.propagate = False
     ch = logging.StreamHandler()
     formatter = RedactingFormatter(list(PII_FIELDS))
     ch.setFormatter(formatter)
