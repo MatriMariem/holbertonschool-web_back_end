78f4a82519ae48ee1662bd71073eb8b2fca3879a
MatriMariem <meriemmatri1994@gmail.com>
2020-11-06 21:17:58 +0100

    Request validation

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -22,12 +22,14 @@ if os.getenv("AUTH_TYPE") == "auth":
 def before_request_func():
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
-    if auth:
-        if not auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
+    if auth is None:
+        pass
+    if auth.require_auth(request.path, excluded_paths):
+        pass
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
