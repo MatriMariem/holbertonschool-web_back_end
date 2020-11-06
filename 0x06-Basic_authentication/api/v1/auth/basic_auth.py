bd1d752eaa6349fc79d88a37e7a450d62e681fae
MatriMariem <meriemmatri1994@gmail.com>
2020-11-06 23:38:26 +0100

    User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,15 @@ class BasicAuth(Auth):
                 'utf-8')
         except Exception as e:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        returns the user email and password from the Base64 decoded value
+        """
+        if (
+           not decoded_base64_authorization_header or
+           not isinstance(decoded_base64_authorization_header, str) or
+           ':' not in decoded_base64_authorization_header):
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':'))
