904d442d87b3f00b78138162dea9d80eff5f163b
MatriMariem <meriemmatri1994@gmail.com>
2020-11-12 23:12:49 +0100

    Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -35,13 +35,11 @@ def before_request_func():
         pass
     else:
         return None
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
     if (not auth.authorization_header(request)
             and not auth.session_cookie(request)):
         abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
     request.current_user = auth.current_user(request)
 
 
