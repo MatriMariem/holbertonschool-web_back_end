fc6da97d4e2ac3365311b8c3a0b83cdab0a2357c
MatriMariem <meriemmatri1994@gmail.com>
2020-11-13 23:12:07 +0100

    it's DB not BD

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""" SessionBDAuth inherits from SessionExpAuth """
+""" SessionDBAuth inherits from SessionExpAuth """
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 
 
-class SessionBDAuth(SessionExpAuth):
-    """ SessionAuth class """
+class SessionDBAuth(SessionExpAuth):
+    """ SessionDBAuth class """
 
     def create_session(self, user_id=None):
         """
@@ -14,9 +14,11 @@ class SessionBDAuth(SessionExpAuth):
         """
         if not user_id:
             return None
-        session_id = self.create_session(user_id)
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
         session_data = {"user_id": user_id, "session_id": session_id}
-        obj = UserSession(session_data)
+        obj = UserSession(**session_data)
         obj.save()
         return session_id
 
@@ -37,6 +39,8 @@ class SessionBDAuth(SessionExpAuth):
         destroys the UserSession
         based on the Session ID from the request cookie
         """
+        if not request:
+            return None
         session_id = self.session_cookie(request)
         objs = UserSession.search({"session_id": session_id})
         if objs and len(objs) > 0:
