d97c7d6926bc139a68ce4ea055f313b2b897dfde
MatriMariem <meriemmatri1994@gmail.com>
2020-11-13 21:09:33 +0100

    fix bug

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -27,7 +27,7 @@ class SessionAuth(Auth):
         """ returns a User instance based on a cookie value: """
         cookie = self.session_cookie(request)
         user_id = self.user_id_for_session_id(cookie)
-        return User.get(userId)
+        return User.get(user_id)
 
     def destroy_session(self, request=None):
         """ deletes the user session / logout """
