c40f8b2ab3504865ed9f8e51c7858db0a40092a7
MatriMariem <meriemmatri1994@gmail.com>
2020-11-13 14:12:40 +0100

    Logout

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -28,7 +28,9 @@ def login() -> str:
     result.set_cookie(os.getenv("SESSION_NAME"), session_id)
     return result
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def logout() -> str:
     """ DELETE /api/v1/auth_session/logout
     """
