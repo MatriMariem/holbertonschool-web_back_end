295f625214790686be546bddcbaba06a5d215676
MatriMariem <meriemmatri1994@gmail.com>
2020-11-14 14:45:55 +0100

    edge cases

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -24,28 +24,30 @@ class SessionDBAuth(SessionExpAuth):
         obj.save()
         return session_id
 
-    def create_session(self, user_id=None):
-        """Create session
-        """
-        if user_id:
-            session_id = super().create_session(user_id)
-            us = UserSession(user_id=user_id, session_id=session_id)
-            us.save()
-            UserSession.save_to_file()
-            return session_id
-
     def user_id_for_session_id(self, session_id=None):
-        """Get user ID from session
+        """
+        returns the User ID
+        by requesting UserSession in the database based on session_id
         """
         if not session_id:
             return None
-        UserSession.load_from_file()
-        users = UserSession.search({'session_id': session_id})
-        for u in users:
-            delta = timedelta(seconds=self.session_duration)
-            if u.created_at + delta < datetime.now():
+        try:
+            UserSession.load_from_file()
+            objs = UserSession.search({"session_id": session_id})
+            if not objs or len(objs) == 0:
                 return None
-            return u.user_id
+            if session_id not in self.user_id_by_session_id:
+                return None
+            if "created_at" not in self.user_id_by_session_id[session_id]:
+                return None
+            limit_date = (timedelta(seconds=self.session_duration) +
+                          self.user_id_by_session_id[session_id]["created_at"])
+            if limit_date < datetime.now():
+                self.destroy_session(request)
+                return None
+            return objs[0].user_id
+        except Exception as e:
+            return None
 
     def destroy_session(self, request=None):
         """
