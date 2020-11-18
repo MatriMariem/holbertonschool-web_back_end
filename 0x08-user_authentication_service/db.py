2e3b98c99a3c61d60a318754b77fe1ef734211c5
MatriMariem <meriemmatri1994@gmail.com>
2020-11-18 15:17:28 +0100

    create user

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from user import Base, User
+from typing import TypeVar
+
+
+class DB:
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """ This method saves the user to the database """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
