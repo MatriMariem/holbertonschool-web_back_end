efe3d87283a4b7a8496cd0e4801f98ecaf7b6a69
MatriMariem <meriemmatri1994@gmail.com>
2020-11-18 18:10:01 +0100

    fix annotation

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -6,7 +6,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from user import Base, User
-from typing import Dict
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
@@ -55,9 +54,8 @@ class DB:
             raise NoResultFound
         else:
             return user
-        return user
 
-    def update_user(self, user_id: int, **kwargs: Dict) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """
         The method will use find_user_by to locate the user to update,
         then will update the user’s attributes
@@ -66,5 +64,8 @@ class DB:
         """
         user = self.find_user_by(id=user_id)
         for k, v in kwargs.items():
-            user.k = v
+            if not hasattr(user, k):
+                raise ValueError
+            else:
+                setattr(user, k, v)
         self._session.commit()
