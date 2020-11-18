d57cc287c308b59e754aebbd6de7c07c56993439
MatriMariem <meriemmatri1994@gmail.com>
2020-11-18 21:10:14 +0100

    generate a random string uuid representation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> str:
@@ -11,7 +12,11 @@ def _hash_password(password: str) -> str:
     The returned string is a salted hash of the input password,
     hashed with bcrypt.hashpw
     """
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())7
+
+def _generate_uuid(self) -> str:
+    """ return a string representation of a new UUID """
+    return str(uuid.uuid4())
 
 
 class Auth:
