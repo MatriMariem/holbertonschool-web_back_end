9a1ffe18f25dd553aea90a551c02a9420878f144
MatriMariem <meriemmatri1994@gmail.com>
2020-11-18 18:07:37 +0100

    remove type annotation

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -36,7 +36,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: Dict) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """
         This method takes in arbitrary keyword arguments
         and returns the first row found in the users table
