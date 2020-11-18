d962fa6672a874140390d167cdaf5ff46d9a8db0
MatriMariem <meriemmatri1994@gmail.com>
2020-11-18 17:58:13 +0100

    Raise exceptions

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -42,6 +42,8 @@ class DB:
         and returns the first row found in the users table
         as filtered by the method’s input arguments.
         """
+        if kwargs is None:
+            raise InvalidRequestError
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
         except InvalidRequestError:
