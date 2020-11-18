1336dc1b3be6bbc8ba96a06ae40c35c801b7593a
MatriMariem <meriemmatri1994@gmail.com>
2020-11-18 18:20:48 +0100

    fix typo: usr instead of user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,4 +1,4 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 """ Password Hashing """
 import bcrypt
 
@@ -8,4 +8,4 @@ def _hash_password(password: str) -> str:
     The returned string is a salted hash of the input password,
     hashed with bcrypt.hashpw
     """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
