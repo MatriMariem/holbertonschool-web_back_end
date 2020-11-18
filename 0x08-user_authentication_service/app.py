cf080e614244d9650eebad572e4a8aa33c14c9ed
MatriMariem <meriemmatri1994@gmail.com>
2020-11-18 20:26:07 +0100

    basic flask app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -11,5 +11,6 @@ def welcome():
     """ returns a message when the route / is requested """
     return jsonify({"message": "Bienvenue"})
 
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
