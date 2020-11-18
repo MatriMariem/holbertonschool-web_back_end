7a2f71d0b682a2f346cc71b7e513e1038d75dc71
MatriMariem <meriemmatri1994@gmail.com>
2020-11-18 17:52:57 +0100

    update user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -6,7 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from user import Base, User
-from typing import TypeVar
+from typing import Dict
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
@@ -34,5 +36,33 @@ class DB:
         self._session.commit()
         return user
 
-    # def find_user_by(self, ):
-    #     pass
+    def find_user_by(self, **kwargs: Dict) -> User:
+        """
+        This method takes in arbitrary keyword arguments
+        and returns the first row found in the users table
+        as filtered by the method’s input arguments.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except InvalidRequestError:
+            raise InvalidRequestError
+        if user is None:
+            raise NoResultFound
+        else:
+            return user
+        return user
+
+    def update_user(self, user_id: int, **kwargs: Dict) -> None:
+        """
+        The method will use find_user_by to locate the user to update,
+        then will update the user’s attributes
+        as passed in the method’s arguments
+        then commit changes to the database.
+        """
+        user = self.find_user_by(id=user_id)
+        try:
+            for k, v in kwargs:
+                user.k = v
+        except Exception as e:
+            raise ValueError
+        self._session.commit()
