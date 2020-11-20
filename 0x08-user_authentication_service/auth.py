b16112b620f05739bd08a20b9dddc369d4b8d80d
MatriMariem <meriemmatri1994@gmail.com>
2020-11-20 08:07:59 +0100

    update user password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -91,7 +91,9 @@ class Auth:
         """ update the user password """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            newp = _hash_password(password)
-            self._db.update_user(user.id, reset_token=None, password=newp)
+            new_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=new_password,
+                                 reset_token=None)
+            return None
         except Exception as e:
             raise ValueError
