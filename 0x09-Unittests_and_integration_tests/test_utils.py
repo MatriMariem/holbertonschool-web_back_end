3f145edff4ceb80a8833697b0dfa3340413a22b3
MatriMariem <meriemmatri1994@gmail.com>
2020-12-27 15:22:47 +0100

    use AssertRaises to check if the function throws an exception in some cases

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,10 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, map, path, result):
         """ A method to test the utils.access_nested_map method """
         self.assertEqual(access_nested_map(map, path), result)
+
+    @parameterized.expand([
+       ({}, ("a",)),
+       ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, map, path):
+        self.assertRaises(KeyError, access_nested_map, map, path)
