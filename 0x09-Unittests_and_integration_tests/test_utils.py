6f5e630606976320f2646693c08f1ead06ba4bda
MatriMariem <meriemmatri1994@gmail.com>
2020-12-27 15:12:46 +0100

    parameterized.expand decorator

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" unittests to test if functions are working as expected """
+from parameterized import parameterized
+import unittest
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ class of unittests methods """
+    @parameterized.expand([
+       ({"a": 1}, ("a",), 1),
+       ({"a": {"b": 2}}, ("a",), {"b": 2}),
+       ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, map, path, result):
+        """ A method to test the utils.access_nested_map method """
+        self.assertEqual(access_nested_map(map, path), result)
