df4363be5918c7f57c6bb5439a1a0496e5b8b83c
MatriMariem <meriemmatri1994@gmail.com>
2021-01-01 00:04:04 +0100

    weighted score

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
 UPDATE users
 @projs = SELECT SUM(score * (SELECT weight FROM projects WHERE id = corrections.project_id)) FROM corrections WHERE corrections.user_id = user_id;
-@ws = SUM(weight) FROM projects WHERE id = (SELECT project_id FROM corrections WHERE user_id = user_id);
+@ws = SELECT SUM(weight) FROM projects WHERE id = (SELECT project_id FROM corrections WHERE user_id = user_id);
 SET average_score = (@projs / @ws) WHERE id = user_id;
 END//
 DELIMITER ;
