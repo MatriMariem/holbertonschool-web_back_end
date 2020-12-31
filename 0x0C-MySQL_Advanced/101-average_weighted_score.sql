f38af0581585db5a7256632b6368c567091acd25
MatriMariem <meriemmatri1994@gmail.com>
2021-01-01 00:33:10 +0100

    weighted score for all students

--- 0x0C-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,10 +1,21 @@
 -- a SQL script that creates a stored procedure
 -- ComputeAverageWeightedScoreForUsers that
 -- computes and store the average weighted score for all students.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
-CALL ComputeAverageWeightedScoreForUser (SELECT id FROM users);
+UPDATE users
+SET average_score = (SELECT SUM(score * (SELECT weight FROM projects WHERE id = corrections.project_id)) / (SELECT sum(weight) FROM projects) FROM corrections WHERE corrections.user_id = user_id) WHERE id = user_id;
+END//
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+SELECT (CALL ComputeAverageWeightedScoreForUser(id)) FROM users;
 END//
 DELIMITER ;
