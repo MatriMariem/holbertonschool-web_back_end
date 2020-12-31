cf0c0fdbe75d1ea95bb94733ee7d8473347a61cb
MatriMariem <meriemmatri1994@gmail.com>
2020-12-31 17:38:33 +0100

    procedure

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -4,7 +4,14 @@ DELIMITER //
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-SET project_id = (SELECT id FROM projects WHERE name = project_name)
-INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
+IF
+EXISTS(SELECT * from projects WHERE name = project_name LIMIT 1)
+THEN
+SET @project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+ELSE
+INSERT INTO projects (name) VALUES (project_name);
+SET @project_id = LAST_INSERT_ID();
+END IF;
+INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, @project_id, score);
 END//
 DELIMITER ;
