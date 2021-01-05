4d926ed9cc4db8717bc036f11bdfaaf2cd83377e
MatriMariem <meriemmatri1994@gmail.com>
2021-01-05 16:06:04 +0100

    ES6 method properties

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const [idx, value] of array.entries()) {
-    /* eslint-disable */
     array[idx] = appendString + value;
-    /* eslint-enable */
   }
 
   return array;
