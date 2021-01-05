b21bb933558f21c9910df996edbd9ea154fbf601
MatriMariem <meriemmatri1994@gmail.com>
2021-01-06 00:37:42 +0100

    then and catch in ES6 promises

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,3 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' })).then(console.log('Got a response from the API')).catch(() => Error());
+  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => Error()).finally(console.log('Got a response from the API'));
 }
