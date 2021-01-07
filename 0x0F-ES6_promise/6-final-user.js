7f3f9f40b497f5e52038691d52d8a094ebf60999
MatriMariem <meriemmatri1994@gmail.com>
2021-01-07 17:25:08 +0100

    multiple promises

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return [ {'status': 'fulfilled' , 'value': await signUpUser(firstName, lastName).then((data) => data)}, {'status': 'rejected', 'value': await uploadPhoto(fileName)}];
+  return [ {'status': 'fulfilled' , 'value': await signUpUser(firstName, lastName).then((data) => data)}, {'status': 'rejected', 'value': await uploadPhoto(fileName).catch( (error) => error)}];
 }
