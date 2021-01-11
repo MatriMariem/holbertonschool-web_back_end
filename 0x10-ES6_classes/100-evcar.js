d88943b8b91458ae646a33d83d2c52b39cbfad37
MatriMariem <meriemmatri1994@gmail.com>
2021-01-11 20:05:02 +0100

    disable eslint

--- 0x10-ES6_classes/100-evcar.js
+++ 0x10-ES6_classes/100-evcar.js
@@ -1,12 +1,12 @@
 import Car from './10-car';
-
+/* eslint-disable */
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  static cloneCar() {
+  cloneCar() {
     return new Car();
   }
 }
