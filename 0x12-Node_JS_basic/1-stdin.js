cad32926accc5c246955e09784b19b37af732cfb
MatriMariem <meriemmatri1994@gmail.com>
2021-01-18 00:38:55 +0100

    process.stdin.on

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,9 +1,12 @@
 console.log("Welcome to Holberton School, what is your name?");
 process.stdin.on('data', (d) => {
-  console.log(`Your name is: ${d.toString().trim()}`);
-
+  if (d.toString().trim() !== '') {
+  process.stdout.write(`Your name is: ${d.toString().trim()}`);
+} else {
+  process.exit();
+}
 })
 process.stdin.on('end', () => {
-  console.log("This important software is now closing");
+  console.log("\nThis important software is now closing");
   process.exit();
 });
