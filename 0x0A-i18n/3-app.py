b4d8ec2286f4898802461ea2c1232a8f464ff28e
MatriMariem <meriemmatri1994@gmail.com>
2021-01-19 18:02:28 +0100

    reset 3-app.py and 3-index.html

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,34 +1,33 @@
-
 #!/usr/bin/env python3
-"""
-Flask application
-"""
+""" a basic flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
-
+from flask_babel import Babel, _
 
 app = Flask(__name__)
+
+
+class Config(object):
+    """ Config class for Babel object """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
 babel = Babel(app)
 
 
-class Config:
-    ''' flask app Config class. '''
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+@app.route('/')
+def hello():
+    """ render a basic html file """
+    return render_template('3-index.html')
 
 
 @babel.localeselector
 def get_locale():
-    """ Determine the best match with our supported languages. """
-    requested_lang = request.accept_languages.best_match(Config.LANGUAGES)
-    return requested_lang
+    """ a function to determine the best match with the supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-app.config.from_object('3-app.Config')
-
-
-@app.route('/')
-def default():
-    """ Returns a 3-index.html template """
-    return render_template('3-index.html')
+if __name__ == '__main__':
+    app.run()
